1cd79824f93b4311c66f1e00bbc191c4ecc603b4
mmgwebi <mihlalimgwebi@gmail.com>
2024-12-20 15:48:41 +0200

    feat: :rocket: update config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,12 +1,40 @@
-module.exports = {
+// module.exports = {
+//   reactStrictMode: true,
+//   pwa: {
+//     dest: 'public',
+//     register: true,
+//     skipWaiting: true,
+//   },
+//   webpack: (config) => {
+//     // Custom webpack configurations can be added here
+//     return config;
+//   },
+// };
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  rewrites: async () => {
+      return [
+      {
+          source: '/api/:path*',
+          destination:
+          process.env.NODE_ENV === 'development'
+              ? 'http://127.0.0.1:8000/api/:path*'
+              : '/api/',
+      },
+      ]
+},
+output: "export",
+}
+
+import withPWA from "next-pwa";
+
+const pwaConfig = withPWA({
+  dest: "public",
+});
+
+export default {
+  ...pwaConfig,
+  ...nextConfig,
   reactStrictMode: true,
-  pwa: {
-    dest: 'public',
-    register: true,
-    skipWaiting: true,
-  },
-  webpack: (config) => {
-    // Custom webpack configurations can be added here
-    return config;
-  },
 };
